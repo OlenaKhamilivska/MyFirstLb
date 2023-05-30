93d0d2b2c25aa4d3acfeb9d5f6b0dd5f7d42a2ff
OlenaKhamilivska <stadni4yk.elena@gmail.com>
2023-05-30 21:48:48 +0100

    added new module

---
+++ mylibrary/src/main/java/com/example/mylibrary/NewClass.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NewClass {
+    public static void showToast (Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
